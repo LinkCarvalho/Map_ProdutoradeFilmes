30de3ab24ae4ad2a7437963239c5f5222b765871
Filipe Carvalho <fcf0133@gmail.com>
2024-04-06 22:22:10 -0300

    Primeiro commit

--- Produtora_de_Filmes/src/Main.java
+++ Produtora_de_Filmes/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
     public static void main(String[] args) {
 
         Filme scottPilgrim = new Filme("Scott Pilgrim contra o mundo", 2010);
+        Empregado Michael_Cera = new Empregado("Michael Cera", "123.456.789-0");
+
+        Michael_Cera.addPapel(Papel.ATOR);
+        Michael_Cera.addFilme(scottPilgrim);
+        scottPilgrim.addAtor(Michael_Cera);
+
+        Michael_Cera.Filmografia();
 
     }
 }
